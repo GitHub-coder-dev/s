ad7db01d3100487f928649b8650de931f4e6917a
GitHub-coder-dev <77382375+GitHub-coder-dev@users.noreply.github.com>
2023-10-21 12:41:36 +0530

    Update CompanyRecord.java

--- src/main/java/com/sarvesh/codetest2/model/CompanyRecord.java
+++ src/main/java/com/sarvesh/codetest2/model/CompanyRecord.java
@@ -8,67 +8,67 @@ import java.time.LocalDate;
 
 @Entity
 public class CompanyRecord {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String companyName;
-    private String companyNumber;
-    private String eventType;
-    private LocalDate eventDate;
-    private String uniqueIdentifier;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+private String companyName;
+private String companyNumber;
+private String eventType;
+private LocalDate eventDate;
+private String uniqueIdentifier;
 
-    public CompanyRecord() {
-    }
+public CompanyRecord() {
+}
 
-    public CompanyRecord(String companyName, String companyNumber, String eventType, LocalDate eventDate, String uniqueIdentifier) {
-        this.companyName = companyName;
-        this.companyNumber = companyNumber;
-        this.eventType = eventType;
-        this.eventDate = eventDate;
-        this.uniqueIdentifier = uniqueIdentifier;
-    }
+public CompanyRecord(String companyName, String companyNumber, String eventType, LocalDate eventDate, String uniqueIdentifier) {
+    this.companyName = companyName;
+    this.companyNumber = companyNumber;
+    this.eventType = eventType;
+    this.eventDate = eventDate;
+    this.uniqueIdentifier = uniqueIdentifier;
+}
 
-    public Long getId() {
-        return id;
-    }
+public Long getId() {
+    return id;
+}
 
-    public String getCompanyName() {
-        return companyName;
-    }
+public String getCompanyName() {
+    return companyName;
+}
 
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
+public void setCompanyName(String companyName) {
+    this.companyName = companyName;
+}
 
-    public String getCompanyNumber() {
-        return companyNumber;
-    }
+public String getCompanyNumber() {
+return companyNumber;
+}
 
-    public void setCompanyNumber(String companyNumber) {
-        this.companyNumber = companyNumber;
-    }
+public void setCompanyNumber(String companyNumber) {
+    this.companyNumber = companyNumber;
+}
 
-    public String getEventType() {
-        return eventType;
-    }
+public String getEventType() {
+return eventType;
+}
 
-    public void setEventType(String eventType) {
-        this.eventType = eventType;
-    }
+public void setEventType(String eventType) {
+    this.eventType = eventType;
+}
 
-    public LocalDate getEventDate() {
-        return eventDate;
-    }
+public LocalDate getEventDate() {
+return eventDate;
+}
 
-    public void setEventDate(LocalDate eventDate) {
-        this.eventDate = eventDate;
-    }
+public void setEventDate(LocalDate eventDate) {
+this.eventDate = eventDate;
+}
 
-    public String getUniqueIdentifier() {
-        return uniqueIdentifier;
-    }
+public String getUniqueIdentifier() {
+   return uniqueIdentifier;
+}
 
-    public void setUniqueIdentifier(String uniqueIdentifier) {
-        this.uniqueIdentifier = uniqueIdentifier;
-    }
+public void setUniqueIdentifier(String uniqueIdentifier) {
+    this.uniqueIdentifier = uniqueIdentifier;
+}
 }
