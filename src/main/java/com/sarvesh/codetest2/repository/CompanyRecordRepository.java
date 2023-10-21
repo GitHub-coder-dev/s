d8789dd1e8bdb95ddc81419720b09770622e4137
GitHub-coder-dev <77382375+GitHub-coder-dev@users.noreply.github.com>
2023-10-21 12:39:29 +0530

    Update CompanyRecordRepository.java

--- src/main/java/com/sarvesh/codetest2/repository/CompanyRecordRepository.java
+++ src/main/java/com/sarvesh/codetest2/repository/CompanyRecordRepository.java
@@ -5,6 +5,4 @@ import com.sarvesh.codetest2.model.CompanyRecord;
 
 public interface CompanyRecordRepository extends CrudRepository<CompanyRecord, Long> {
 
-//Create a repository interface for the Record entity to interact with the database using Spring Data JPA.
-
 }
